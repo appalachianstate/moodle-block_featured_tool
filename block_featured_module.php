023d489ee6f05dc3a747ece4f6943c31c08f6224
derbear444 <dcwilson0901@gmail.com>
2023-06-07 12:49:09 -0400

    Starting to think maybe this isn't the way to go about it

--- block_featured_module.php
+++ block_featured_module.php
@@ -53,7 +53,7 @@ class block_featured_module extends block_base {
         $this->content->footer = '';
 
         if (get_config('block_featured_module', 'featuredmedia')) {
-            $termspdf = $this->theme->setting_file_url('featuredmedia', 'featuredmedia');
+            $termspdf = $theme->setting_file_url('logo', 'logo');
             $this->content->text .= html_writer::tag('a', '', array('href'=>$termspdf));;
         } else {
             $text = 'This is where featured posts will go.';

3741d69abec91a98b3e87f23a19c2fcc042fbaf5
derbear444 <dcwilson0901@gmail.com>
2023-06-08 09:39:04 -0400

    Going to try out the code checkers on my code to see what they say

--- block_featured_module.php
+++ block_featured_module.php
@@ -54,12 +54,33 @@ class block_featured_module extends block_base {
         $this->content->icons = array();
         $this->content->footer = '';
 
-        if (!empty($this->config->text)) {
-            $this->content->text = $this->config->text;
+        require_once('myform.php');
+        //Instantiate simplehtml_form
+        $mform = new \my_form();
+        if ($mform->is_cancelled()) {
+            // If there is a cancel element on the form, and it was pressed,
+            // then the `is_cancelled()` function will return true.
+            // You can handle the cancel operation here.
+        } else if ($fromform = $mform->get_data()) {
+            // When the form is submitted, and the data is successfully validated,
+            // the `get_data()` function will return the data posted in the form.
         } else {
-            $text = 'This is where featured posts will go.';
-            $this->content->text = $text;
+            // This branch is executed if the form is submitted but the data doesn't
+            // validate and the form should be redisplayed or on the first display of the form.
+
+            // Set anydefault data (if any).
+            $mform->set_data($toform);
+
+            // Display the form.
+            $this->content->text = $mform->render();
         }
+
+        //if (!empty($this->config->text)) {
+        //    $this->content->text = $this->config->text;
+        //} else {
+        //    $text = 'This is where featured posts will go.';
+        //    $this->content->text = $text;
+        //}
 
         return $this->content;
     }

8d4aa7fcc23068a7474f3b6cf0e1eaea61236444
derbear444 <dcwilson0901@gmail.com>
2023-06-09 15:49:27 -0400

    Changing lib.php to match html's

--- lib.php
+++ lib.php
@@ -15,104 +15,121 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Plugin functions.
+ * Form for editing HTML block instances.
  *
- * @package     block_featured_tool
  * @copyright   2023 Derek Wilson <wilsondc5@appstate.edu>
- * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package   block_featured_tool
+ * @category  files
+ * @param stdClass $course course object
+ * @param stdClass $birecord_or_cm block instance record
+ * @param stdClass $context context object
+ * @param string $filearea file area
+ * @param array $args extra arguments
+ * @param bool $forcedownload whether or not force download
+ * @param array $options additional options affecting the file serving
+ * @return bool
+ * @todo MDL-36050 improve capability check on stick blocks, so we can check user capability before sending images.
  */
+function block_featured_tool_pluginfile($course, $birecord_or_cm, $context, $filearea, $args, $forcedownload, array $options=array()) {
+    global $DB, $CFG, $USER;
 
-defined('MOODLE_INTERNAL') || die;
-
-require_once($CFG->dirroot . '/repository/lib.php');
-
-/**
- * Add featured_tool instance.
- *
- * @param stdClass $data
- * @param block_featured_tool_edit_form $mform
- * @return int new featured_tool instance id
- */
-function featured_tool_add_instance($data, $mform = null) {
-    global $CFG, $DB;
-    require_once("$CFG->libdir/resourcelib.php");
-
-    if ($mform) {
-        $editoroptions = array(
-                'subdirs' => 0,
-                'maxbytes' => $CFG->maxbytes,
-                'maxfiles' => -1,
-                'changeformat' => 1,
-                'context' => $CFG->context,
-                'noclean' => 1,
-                'trusttext' => 0
-        );
-        $formattedtypes = array('featuredmedia');
-        foreach ($formattedtypes as $type) {
-            ${'temp_' . $type} = $data->$type;
-            $data->$type = ${'temp_' . $type}['text'];
-            $data->{$type . 'format'} = ${'temp_' . $type}['format'];
-        }
+    if ($context->contextlevel != CONTEXT_BLOCK) {
+        send_file_not_found();
     }
 
-    $data->id = $DB->insert_record('featuredtool', $data);
-
-    // We need to use context now, so we need to make sure all needed info is already in db.
-    #$DB->set_field('course_modules', 'instance', $data->id, array('id' => $data->coursemodule));
-
-    if ($mform) {
-        foreach ($formattedtypes as $type) {
-            if (!empty(${'temp_' . $type}['itemid'])) {
-                $draftitemid = ${'temp_' . $type}['itemid'];
-                $data->$type = file_save_draft_area_files($draftitemid, $context->id, 'block_featured_tool', $type, 0, $editoroptions, $data->$type);
+    // If block is in course context, then check if user has capability to access course.
+    if ($context->get_course_context(false)) {
+        require_course_login($course);
+    } else if ($CFG->forcelogin) {
+        require_login();
+    } else {
+        // Get parent context and see if user have proper permission.
+        $parentcontext = $context->get_parent_context();
+        if ($parentcontext->contextlevel === CONTEXT_COURSECAT) {
+            // Check if category is visible and user can view this category.
+            if (!core_course_category::get($parentcontext->instanceid, IGNORE_MISSING)) {
+                send_file_not_found();
             }
+        } else if ($parentcontext->contextlevel === CONTEXT_USER && $parentcontext->instanceid != $USER->id) {
+            // The block is in the context of a user, it is only visible to the user who it belongs to.
+            send_file_not_found();
         }
+        // At this point there is no way to check SYSTEM context, so ignoring it.
     }
 
-    $DB->update_record('featuredtool', $data);
+    if ($filearea !== 'content') {
+        send_file_not_found();
+    }
 
-    return $data->id;
+    $fs = get_file_storage();
+
+    $filename = array_pop($args);
+    $filepath = $args ? '/'.implode('/', $args).'/' : '/';
+
+    if (!$file = $fs->get_file($context->id, 'block_html', 'content', 0, $filepath, $filename) or $file->is_directory()) {
+        send_file_not_found();
+    }
+
+    if ($parentcontext = context::instance_by_id($birecord_or_cm->parentcontextid, IGNORE_MISSING)) {
+        if ($parentcontext->contextlevel == CONTEXT_USER) {
+            // force download on all personal pages including /my/
+            //because we do not have reliable way to find out from where this is used
+            $forcedownload = true;
+        }
+    } else {
+        // weird, there should be parent context, better force dowload then
+        $forcedownload = true;
+    }
+
+    // NOTE: it woudl be nice to have file revisions here, for now rely on standard file lifetime,
+    //       do not lower it because the files are dispalyed very often.
+    \core\session\manager::write_close();
+    send_stored_file($file, null, 0, $forcedownload, $options);
 }
 
 /**
- * Update featured_tool instance.
- *
- * @param object $data
- * @param object $mform
- * @return bool true
+ * Perform global search replace such as when migrating site to new URL.
+ * @param  $search
+ * @param  $replace
+ * @return void
  */
-function featured_tool_update_instance($data, $mform) {
-    global $CFG, $DB;
-    require_once("$CFG->libdir/resourcelib.php");
+function block_featured_tool_global_db_replace($search, $replace) {
+    global $DB;
 
-    $data->timemodified = time();
-    $data->id = $data->instance;
-    $data->revision++;
-
-    $editoroptions = array(
-            'subdirs' => 0,
-            'maxbytes' => $CFG->maxbytes,
-            'maxfiles' => -1,
-            'changeformat' => 1,
-            'context' => $CFG->context,
-            'noclean' => 1,
-            'trusttext' => 0
-    );
-
-    $formattedtypes = array('featuredmedia');
-    foreach ($formattedtypes as $type) {
-        ${'temp_' . $type} = $data->$type;
-        $data->$type = ${'temp_' . $type}['text'];
-        $data->{$type . 'format'} = ${'temp_' . $type}['format'];
-
-        $draftitemid = ${'temp_' . $type}['itemid'];
-
-        if ($draftitemid) {
-            $data->$type = file_save_draft_area_files($draftitemid, $context->id, 'block_featured_tool', $type, 0, $editoroptions, $data->$type);
+    $instances = $DB->get_recordset('block_instances', array('blockname' => 'html'));
+    foreach ($instances as $instance) {
+        // TODO: intentionally hardcoded until MDL-26800 is fixed
+        $config = unserialize_object(base64_decode($instance->configdata));
+        if (isset($config->text) and is_string($config->text)) {
+            $config->text = str_replace($search, $replace, $config->text);
+            $DB->update_record('block_instances', ['id' => $instance->id,
+                    'configdata' => base64_encode(serialize($config)), 'timemodified' => time()]);
         }
     }
+    $instances->close();
+}
 
-    $DB->update_record('featuredtool', $data);
+/**
+ * Given an array with a file path, it returns the itemid and the filepath for the defined filearea.
+ *
+ * @param  string $filearea The filearea.
+ * @param  array  $args The path (the part after the filearea and before the filename).
+ * @return array The itemid and the filepath inside the $args path, for the defined filearea.
+ */
+function block_featured_tool_get_path_from_pluginfile(string $filearea, array $args) : array {
+    // This block never has an itemid (the number represents the revision but it's not stored in database).
+    array_shift($args);
 
-    return true;
+    // Get the filepath.
+    if (empty($args)) {
+        $filepath = '/';
+    } else {
+        $filepath = '/' . implode('/', $args) . '/';
+    }
+
+    return [
+            'itemid' => 0,
+            'filepath' => $filepath,
+    ];
 }

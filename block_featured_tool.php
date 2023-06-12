e989cd260a58bc45e1341147fddaa401011c26a0
derbear444 <dcwilson0901@gmail.com>
2023-06-12 13:36:44 -0400

    Minor change to how I'm accessing

--- block_featured_tool.php
+++ block_featured_tool.php
@@ -143,7 +143,7 @@ class block_featured_tool extends block_base {
         if ($draftitemid) {
             #$config->text = file_save_draft_area_files($draftitemid, $this->context->id, 'block_featured_tool', $type, 0, $editoroptions, $data->$type);
         }
-        $config->text = $data['featuredmedia'];
+        $config->text = $data->featuredmedia;
         // Move embedded files into a proper filearea and adjust HTML links to match
         #$config->text = file_save_draft_area_files($data->text['itemid'], $this->context->id, 'block_featured_tool', 'featuredmedia', 0, $editoroptions, $data->text['text']);
         $config->format = $data->text['format'];

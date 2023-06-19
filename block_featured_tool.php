6beb8b5c75a313b18a652e6cad2d87cb59b5d33a
derbear444 <dcwilson0901@gmail.com>
2023-06-19 09:46:46 -0400

    Trying to implement showing content in block

--- block_featured_tool.php
+++ block_featured_tool.php
@@ -65,7 +65,9 @@ class block_featured_tool extends block_base {
             $this->content->footer = '';
 
             if (!empty($this->config->media)) {
-                $this->content->text = $this->config->media;
+                $this->content->text = file_rewrite_pluginfile_urls($this->config->media, 'pluginfile.php', $this->context->id, 'block_featured_tool', 'content', null);
+                $format = FORMAT_HTML;
+                $this->content->text = format_text($this->config->media, $format, null);
             } else {
                 $text = 'Please define the content text in /blocks/featured_tool/block_featured_tool.php.';
                 $this->content->text = $text;

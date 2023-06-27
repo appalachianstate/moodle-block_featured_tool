c03ece380f41b7b8c46a64221484652b5cc5f7c3
derbear444 <dcwilson0901@gmail.com>
2023-06-27 17:57:35 -0400

    Trying a method of dynamic variable assignment 2

--- block_featured_tool.php
+++ block_featured_tool.php
@@ -156,7 +156,7 @@ class block_featured_tool extends block_base {
                 // Move embedded files into a proper filearea and adjust HTML links to match
                 $config->text[$key] = file_save_draft_area_files($text['itemid'], $sitecontext->id,
                         'block_featured_tool', ('content' . $key), 0, array('subdirs'=>true), $text['text']);
-                ${'config->text'} . $idx = $config->text[$key];
+                ${'config->text'.$idx} = $config->text[$key];
                 // If a subtitle exists for this block, store it in the same index of the subtitle array
                 // Otherwise, it stores a default subtitle
                 $config->subtitle[$key] = !empty($data->subtitle[$idx]) ? $data->subtitle[$idx] : "Announcement";

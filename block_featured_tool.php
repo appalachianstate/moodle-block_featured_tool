4006d887309d74873ba86592e22c0130662baf58
derbear444 <dcwilson0901@gmail.com>
2023-06-27 16:19:19 -0400

    Removed the unnecessary config->thumbnail variable, since I'm grabbing thumbnails directly from the file system

--- block_featured_tool.php
+++ block_featured_tool.php
@@ -147,7 +147,6 @@ class block_featured_tool extends block_base {
         // Save only area files that have something in them and store them
         $config->text = array();
         $config->subtitle = array();
-        $config->thumbnail = array();
         foreach ($data->text as $text) {
             if (!empty($text) && !empty($text['text'])) {
                 // Generates the key of where the text will be stored in the final text array
@@ -162,7 +161,7 @@ class block_featured_tool extends block_base {
                 // If a thumbnail exists for this block, move the thumbnail into a proper filearea and adjust HTML link to match
                 if (!empty($data->thumbnail[$key])) {
                     $thumbnail = $data->thumbnail[$key];
-                    $config->thumbnail[$key] = file_save_draft_area_files($thumbnail, $sitecontext->id,
+                    file_save_draft_area_files($thumbnail, $sitecontext->id,
                     'block_featured_tool', ('thumbnail' . $key), 0, $thumbnailoptions);
                 }
             }

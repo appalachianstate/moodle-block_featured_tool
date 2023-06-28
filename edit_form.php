9933015f18c0e6f99f02a77add552f3a66876bf0
derbear444 <dcwilson0901@gmail.com>
2023-06-28 10:07:37 -0400

    Added print to understand what's going on

--- edit_form.php
+++ edit_form.php
@@ -134,11 +134,12 @@ class block_featured_tool_edit_form extends block_edit_form {
                 ${'text' . $idx} = $text;
                 $draftid_editor = file_get_submitted_draft_itemid('config_text' . $idx);
                 $editor_spec = 'config_text' . $idx;
-                $defaults->$editor_spec['text'] =
+                $defaults->$editor_spec[$text_var] =
                         file_prepare_draft_area($draftid_editor, $sitecontext->id, 'block_featured_tool', 'content0', 0,
                                 array('subdirs' => true), $text);
                 $defaults->$editor_spec['itemid'] = $draftid_editor;
                 $defaults->$editor_spec['format'] = FORMAT_HTML;
+                print_object($defaults);
                 // Remove the thumbnail from the config so that parent::set_data doesn't empty it.
                 unset($this->block->config->${'text'.$idx});
             }

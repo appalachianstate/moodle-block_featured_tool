62eb8f61a35c5db6a87709f37bf88ab79286d98f
derbear444 <dcwilson0901@gmail.com>
2023-06-28 11:08:44 -0400

    Switching out part of defaults to see if it is the problem

--- edit_form.php
+++ edit_form.php
@@ -136,11 +136,11 @@ class block_featured_tool_edit_form extends block_edit_form {
                 $config_text_num = 'config_text' . $idx;
                 $draftid_editor = file_get_submitted_draft_itemid($config_text_num);
 
-                $defaults->{$config_text_num}['text'] =
+                $defaults->config_text0['text'] =
                         file_prepare_draft_area($draftid_editor, $sitecontext->id, 'block_featured_tool', ('content' . $idx), 0,
                                 array('subdirs' => true), $text);
-                $defaults->{$config_text_num}['itemid'] = $draftid_editor;
-                $defaults->{$config_text_num}['format'] = FORMAT_HTML;
+                $defaults->config_text0['itemid'] = $draftid_editor;
+                $defaults->config_text0['format'] = FORMAT_HTML;
                 print_object($defaults);
                 // Remove the text from the config so that parent::set_data doesn't empty it.
                 print_object($this->block->config);
